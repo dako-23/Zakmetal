aa4ad0bbd6bfc0b3da7569c45482514c913ad8e7
Yordan Grigorov <iordan.megaparts@gmail.com>
2025-07-03 16:56:38 +0300

    Add for-you section data and use react-icons
    Introduced a new constants file for the for-you section data and refactored ForYou component to render items dynamically using this data. Added react-icons as a dependency and updated styles to support icon rendering.

--- src/components/for-you/For-you.jsx
+++ src/components/for-you/For-you.jsx
@@ -1,20 +1,22 @@
 import './For-you.css'
+import { data } from '../constants/for-you-data.js';
 
 export default function ForYou() {
     return (
         <section className="section section-for-you">
             <div className="inner">
-                <div className="content">
-                    <span className="icon">
-                        ☎
-                    </span>
-                    <div className="info">
-                        <h3>НАЙ-ДОБРА ЦЕНА ВЕДНАГА</h3>
-                        <p>Професионално оценяване</p>
-                        <p>Получете справедлива оферта за стария си автомобил още днес — без скрити такси и излишно чакане.
-                        </p>
+                {data.map(({ icon: Icon, title, subTitle, text }) =>
+                    <div className="content" key={title}>
+                        <span className="icon">
+                            <Icon/>
+                        </span>
+                        <div className="info">
+                            <h3>{title}</h3>
+                            <p>{subTitle}</p>
+                            <p>{text}</p>
+                        </div>
                     </div>
-                </div>
+                )}
             </div>
         </section>
     );
